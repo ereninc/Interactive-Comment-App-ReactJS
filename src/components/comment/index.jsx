4a4a5b4a26496dfb3bb1d74d189e46949ab639c8
Eren <erencan_inanci@hotmail.com>
2023-02-26 05:00:16 +0300

    reply section update

--- src/components/comment/index.jsx
+++ src/components/comment/index.jsx
@@ -2,19 +2,42 @@ import React from "react";
 import Reactions from "./reactions";
 import Header from "./header";
 import Content from "./content";
-import { useComment } from "../../hooks/comment/useComment";
+import {
+  useComment,
+  CommentContextProvider,
+} from "../../hooks/comment/useComment";
 import styles from "./styles.module.scss";
 
 export default function Comment() {
-  const comment = useComment();
-  console.log(comment);
+  const {
+    currentUser,
+    comment: { replies },
+  } = useComment();
+
   return (
-    <div className={styles.commentWrapper}>
-      <Reactions />
-      <div className={styles.commentContentArea}>
-        <Header />
-        <Content />
+    <>
+      <div className={styles.commentWrapper}>
+        <Reactions />
+        <div className={styles.commentContentArea}>
+          <Header />
+          <Content />
+        </div>
       </div>
-    </div>
+
+      {replies?.length > 0 && (
+        <div className={styles.repliesWrapper}>
+          {replies.map((reply) => {
+            return (
+              <CommentContextProvider
+                data={{ comment: reply, currentUser: currentUser }}
+                key={reply.id}
+              >
+                <Comment />
+              </CommentContextProvider>
+            );
+          })}
+        </div>
+      )}
+    </>
   );
 }
